3e16a9195eb1f7a34bf99ee6fb7e26c660efbef7
tehcneko <chsqwyx@gmail.com>
2025-10-17 20:11:27 +0800

    Fix update layout text

--- TMessagesProj/src/main/java/org/telegram/ui/Components/UpdateLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/UpdateLayout.java
@@ -138,7 +138,7 @@ public class UpdateLayout extends IUpdateLayout {
             updateTextViews[i].setGravity(Gravity.LEFT);
             updateLayout.addView(updateTextViews[i], LayoutHelper.createFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER_VERTICAL, 74, 0, 0, 0));
         }
-        updateTextViews[0].setText(LocaleController.getString(R.string.AppUpdate));
+        updateTextViews[0].setText(LocaleController.getString(R.string.UpdateNekogram));
         updateTextViews[1].setAlpha(0f);
         updateTextViews[1].setVisibility(View.GONE);
 
@@ -173,7 +173,7 @@ public class UpdateLayout extends IUpdateLayout {
                     showSize = false;
                 } else {
                     updateLayoutIcon.setIcon(MediaActionDrawable.ICON_DOWNLOAD, true, animated);
-                    setUpdateText(LocaleController.getString(R.string.AppUpdate), animated);
+                    setUpdateText(LocaleController.getString(R.string.UpdateNekogram), animated);
                     showSize = true;
                 }
             }
--- TMessagesProj/src/main/java/org/telegram/ui/Stories/PeerStoriesView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Stories/PeerStoriesView.java
@@ -3708,7 +3708,7 @@ public class PeerStoriesView extends SizeNotifierFrameLayout implements Notifica
 
         TextView buttonTextView = new TextView(getContext());
         ScaleStateListAnimator.apply(buttonTextView);
-        buttonTextView.setText(getString(R.string.AppUpdate));
+        buttonTextView.setText(getString(R.string.UpdateNekogram));
         buttonTextView.setTextColor(Theme.getColor(Theme.key_featuredStickers_buttonText, resourcesProvider));
         buttonTextView.setPadding(AndroidUtilities.dp(16), AndroidUtilities.dp(12), AndroidUtilities.dp(16), AndroidUtilities.dp(12));
         buttonTextView.setGravity(Gravity.CENTER);
